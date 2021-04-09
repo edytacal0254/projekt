27137b01acf3bbb56b48797838cc6ece4b1887d8
edytacal0254 <44200308+edytacal0254@users.noreply.github.com>
2021-04-09 13:00:44 +0200

    first

---
+++ generator.py
@@ -0,0 +1,124 @@
+import os
+from PIL import Image
+import math
+import random
+import csv
+
+# ________________________cords idx_c meaning
+# 0 - centered
+# 1 - top left corner
+# 2 - bottom right corner
+# 3 - left side
+# 4 - bottom side
+# 5 - random
+
+# _____________________________ scales
+scale = [0.25, 0.125, 0.0725]
+s_1 = True  # 100% scale, centered
+
+# _____________________________images and their paths
+input_dir = os.getcwd() + "\\objects"
+output_dir = os.getcwd() + "\\generated"
+backgrounds_dir = os.getcwd() + "\\backgrounds"
+
+nr_of_generated = len(os.listdir(output_dir))
+
+basic_img_list = os.listdir(input_dir)
+basic_img_list_len = len(basic_img_list)
+
+backgrounds_list = os.listdir(backgrounds_dir)
+
+# ___________________________________________________________________ csv
+data_file = open(os.getcwd() + "\\data.csv", "w", encoding='utf-8', newline='')
+data_writer = csv.writer(data_file)
+
+# __________________________________________________________________________________________________
+for idx_bon, basic_object_name in enumerate(basic_img_list):
+    print("Processing [" + str(idx_bon + 1) + "/" + str(basic_img_list_len) + "]")
+
+    basic_object = Image.open(input_dir + "\\" + basic_object_name)
+    width, height = basic_object.size
+
+    # for testing purposes
+    #if idx_bon == 2:
+    #    break
+
+    # if not scaled down (actually objects are scaled down if they not fit in Full HD)
+    if s_1:
+        new_s = 1
+        # checking if object fits in Full HD (in input objects only one dimension can be too big)
+        if width > 1920:
+            new_s = 1920 / width
+        if height > 1080:
+            new_s = 1080 / height
+        # calc new dimensions and scale down________________________________
+        new_width = math.floor(width * new_s)
+        new_height = math.floor(height * new_s)
+        half_new_width = math.floor(new_width / 2)
+        half_new_height = math.floor(new_height / 2)
+
+        scaled_object = basic_object.resize((new_width, new_height))
+
+        # coordinates - only centered in that case_________________________
+        cord_c = (960 - half_new_width, 540 - half_new_height)
+
+        for bg_name in backgrounds_list:
+            bg_img = Image.open(backgrounds_dir + "\\" + bg_name)
+
+            tmp = basic_object_name.partition('.')
+            new_name = tmp[0] + "_" + str(nr_of_generated) + ".jpg"
+
+            # paste object to bg, convert to jpeg save____________________________
+            new_gen_img = bg_img.copy()
+            new_gen_img.paste(scaled_object, cord_c, scaled_object)
+            converted_img = new_gen_img.convert("RGB")
+            converted_img.save(output_dir + "\\" + new_name, "jpeg")
+            # csv data___________________________________________________________
+            data_row = [new_name, basic_object_name, bg_name, 100, 0]   # 100 czy new_s?
+            data_writer.writerow(data_row)
+
+            nr_of_generated += 1
+    # end of: if s_1 (unique case) _________________________________________________________
+
+    for s in scale:
+        # calc new dimensions and scale down________________________________
+        new_width = math.floor(width * s)
+        new_height = math.floor(height * s)
+        half_new_width = math.floor(new_width/2)
+        half_new_height = math.floor(new_height/2)
+
+        scaled_object = basic_object.resize((new_width, new_height))
+
+        # coordinates____________________________________________
+        cord_c = (960 - half_new_width, 540 - half_new_height)
+        cord_tl = (0, 0)
+        cord_br = ((1920-new_width), (1080-new_height))
+        cord_l = (0, 540 - half_new_height)
+        cord_b = (960 - half_new_width, 0)
+
+        random.seed(nr_of_generated)
+        rand_x = random.randint(half_new_width, 1920 - half_new_width)
+        rand_y = random.randint(half_new_height, 1080 - half_new_height)
+        cord_rand = (rand_x - half_new_width, rand_y - half_new_height)
+
+        cords = (cord_c, cord_tl, cord_br, cord_l, cord_b, cord_rand)
+
+        for bg_name in backgrounds_list:
+            bg_img = Image.open(backgrounds_dir + "\\" + bg_name)
+
+            for idx_c, c in enumerate(cords):
+                tmp = basic_object_name.partition('.')
+                new_name = tmp[0] + "_" + str(nr_of_generated) + ".jpg"
+                # paste object to bg, convert to jpeg save____________________________
+                new_gen_img = bg_img.copy()
+                new_gen_img.paste(scaled_object, c, scaled_object)
+                converted_img = new_gen_img.convert("RGB")
+                converted_img.save(output_dir + "\\" + new_name, "jpeg")
+
+                data_row = [new_name, basic_object_name, bg_name, s, idx_c]
+                data_writer.writerow(data_row)
+
+                nr_of_generated += 1
+
+data_file.close()
+print("Generated " + str(len(os.listdir(output_dir))) + " images")
